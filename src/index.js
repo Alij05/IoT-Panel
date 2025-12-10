9b7b26ebdd91278dec3cc786ccc6930ff4a133b6
Ali Jalilvand <ali.jalilvand2203@gmail.com>
2025-12-10 10:20:24 +0330

    Set axios.defaults.withCredentials = true to enable sending cookies with requests

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './custom.css'
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
