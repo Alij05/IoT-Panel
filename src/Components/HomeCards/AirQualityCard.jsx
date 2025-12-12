21013dd895bd2fa2bf17f89678105f81dffc6b7d
Ali Jalilvand <ali.jalilvand2203@gmail.com>
2025-12-12 16:39:55 +0330

    Change UI of activating 2FA

--- src/Components/HomeCards/AirQualityCard.jsx
+++ src/Components/HomeCards/AirQualityCard.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import GaugeChart from 'react-gauge-chart';
 import './AirQualityCard.css';
+import { useState } from 'react';
 
-const AirQualityCard = ({ product, deviceState, deviceInfo }) => {
+const AirQualityCard = ({ product, isUserAdmin, deviceState, deviceInfo, deviceStatus }) => {
+    const [isShowMoreInfo, setIsShowMoreInfo] = useState(false);
+
     const maxAqi = 500;
 
     const safeValue = Number(deviceState);
@@ -10,11 +13,16 @@ const AirQualityCard = ({ product, deviceState, deviceInfo }) => {
 
     return (
         <div className="home-box aqi-card">
-            <div style={{ display: "flex", gap: "15px 20px", fontSize: "18px", color: "black", flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center' }}>
-                <span>مکان : {product?.deviceLocation || '-'}</span>
-                |
-                <span>مالک : {product?.owner || '-'}</span>
+            <div className='more-info' onClick={() => setIsShowMoreInfo(true)}>
+                ...
             </div>
+
+            <div
+                className={`status-dot ${deviceStatus?.includes('Sensor status publish OK') || deviceStatus?.includes('Heartbeat - device online')
+                    ? 'online'
+                    : 'offline'
+                    }`}
+            ></div>
 
             <GaugeChart
                 id={`aqi-gauge-${product?.entity_id}`}
@@ -22,7 +30,7 @@ const AirQualityCard = ({ product, deviceState, deviceInfo }) => {
                 arcsLength={[0.1, 0.1, 0.1, 0.2, 0.2, 0.3]}
                 colors={["#00e400", "#ffff00", "#ff7e00", "#ff0000", "#8f3f97", "#7e0023"]}
                 percent={percent}
-                arcWidth={0.3}
+                arcWidth={0.2}
                 needleColor="#ffffff"
                 textColor="#ffffff"
                 needleBaseColor="#ffffff"
